16670f7b34b337c3dc2b44d7294b79228e4f0c07
Dasaav <111015245+Dasaav-dsv@users.noreply.github.com>
2023-07-18 00:13:21 +0200

    Update Pattern16.h

--- include/Pattern16.h
+++ include/Pattern16.h
@@ -1,14 +1,16 @@
 #pragma once
 
+#include <string>
+
 #include "scanners/base.h"
 
 namespace Pattern16 {
-	void* scan(void* regionStart, size_t regionSize, std::string signature, const Impl::Frequencies16& frequencies = Impl::loadFrequencyCache()) {
+	void* find(void* regionStart, size_t regionSize, std::string signature, const Impl::Frequencies16& frequencies = Impl::loadFrequencyCache()) {
 		auto signature_u8 = Impl::processSignatureString(signature);
 		return (void*)Impl::scan(regionStart, regionSize, signature_u8, frequencies);
 	}
 
-	const void* scan(const void* regionStart, size_t regionSize, std::string signature, const Impl::Frequencies16& frequencies = Impl::loadFrequencyCache()) {
+	const void* find(const void* regionStart, size_t regionSize, std::string signature, const Impl::Frequencies16& frequencies = Impl::loadFrequencyCache()) {
 		auto signature_u8 = Impl::processSignatureString(signature);
 		return Impl::scan(regionStart, regionSize, signature_u8, frequencies);
 	}
